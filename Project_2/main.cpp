c21798e7ba5b7fd3acaa37a80c6f3a1cbfc6de3e
Dorthea Gjestvang <dorthea.gjestvang@gmail.com>
2016-09-22 12:34:36 +0200

    Laget rho

--- Project_2/main.cpp
+++ Project_2/main.cpp
@@ -6,5 +6,21 @@ using namespace std;
 
 int main(){
 
+
+    //Lager rho-arrayen
+    double* rho = new double[N+1];
+    rho[0] = 0.0;
+    rho[N] = 1000.0; //Approx infinity
+
+    double h; h = (rho[N] - rho[0])/(N+1); //N+1 eller N?
+
+    //Lager rho, looper til N-1
+    for(int i = 1; i < N; i++)
+    {
+        rho[i] = rho[0] + i*h;
+    }
+
+    //Setter opp matri
+
     return 0;
 }
