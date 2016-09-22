003d33cf0d91571d984524d11838b1c60182f07f
Dorthea Gjestvang <dorthea.gjestvang@gmail.com>
2016-09-22 13:52:02 +0200

    Merge

--- Project_2/jacobian_rotation.cpp
+++ Project_2/jacobian_rotation.cpp
@@ -12,13 +12,13 @@ void Jacobi_rotate(mat A, mat R, int k, int l, int N)
         tau = (A(l,l) - A(k,k))/(2*A(k,l));
         if (tau >= 0)
         {
-            t = 1.0/(tau + sqrt(1.0 + pow(tau,2.0)));
+            t = 1.0/(tau + sqrt(1.0 + tau*tau));
         }
         else
         {
-            t = -1.0/(-tau + sqrt(1 + pow(tau,2.0)));
+            t = -1.0/(-tau + sqrt(1 + tau*tau));
         }
-        c = 1.0/sqrt(1 + pow(t,2.0));
+        c = 1.0/sqrt(1 + t*t);
         s = c*t;
     }
     else
@@ -28,5 +28,14 @@ void Jacobi_rotate(mat A, mat R, int k, int l, int N)
     }
     // Perform the rotation
     A(k,k) = A(k,k)*c*c - 2*A(k,l)*c*s + A(l,l)*s*s;
-    A(l,l)
+    A(l,l) = A(l,l)*c*c + 2*A(k,l)*c*s + A(k,k)*s*s;
+    A(k,l) = A(l,k) = 0; // Set the previously biggest elements to 0
+    for (int i = 0; i < N-1; i++){
+        if (i != k && i != l) {
+            A(i,k) = A(i,k)*c - A(i,l)*s;
+            A(i,l) = A(i,l)*c + A(i,k)*s;
+            A(k,i) = A(i,k);
+            A(l,i) = A(i,l);
+        }
+    }
 }
