7cb6a59242af4bb1d89e1e2ae56cc250f439fdf7
Richard Fauli <richaraf@student.matnat.uio.no>
2016-09-23 11:03:02 +0200

    Added various comments.

--- Project_2/orthogonality_check.cpp
+++ Project_2/orthogonality_check.cpp
@@ -9,18 +9,19 @@ using namespace std;
 void orthogonality_check_func(mat R, double eps){
     int R_cols = R.n_cols;
     int R_rows = R.n_rows;
+    // prepare column vectors
     vec col1 = zeros<vec>(R_rows);
     vec col2 = zeros<vec>(R_rows);
     for (int c1 = 0; c1 < R_cols; c1++){
         for (int i = 0; i < R_rows; i++){
-            col1(i) = R(c1,i);
+            col1(i) = R(c1,i); // generate column i
             }
         for (int c2 = 0; c2 < R_cols; c2++){
             if (c1 != c2){
             for (int j = 0; j < R_rows; j++){
-                col2(j) = R(c2,j);
+                col2(j) = R(c2,j); // generate column j
             }
-            double dotproduct = dot(col1,col2);
+            double dotproduct = dot(col1,col2); // calculate dot product between column i and j
             if (dotproduct > eps){
                 cout << "Error: dot product not conserved." << endl;
             }
