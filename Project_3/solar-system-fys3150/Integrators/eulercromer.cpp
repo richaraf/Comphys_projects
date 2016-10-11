bd9a4802068cc92b7694772871be884312e36d1f
Richard Fauli <Richard Fauli>
2016-10-11 11:39:26 +0200

    added larger structure

---
+++ Project_3/solar-system-fys3150/Integrators/eulercromer.cpp
@@ -0,0 +1,32 @@
+#include "eulercromer.h"
+#include "../system.h"
+#include <iostream>
+
+EulerCromer::EulerCromer(System* system)
+    : Integrator(system) {
+}
+
+void EulerCromer::integrateOneStep(std::vector<Particle*> particles) {
+    m_system->computeForces();
+    for (int i=0; i<particles.size(); i++) {
+        Particle *p = particles.at(i);
+        //std::cout << p->getVelocity() << std::endl; // check to see what prints
+
+        /*
+         * This is where you need to update the positions and the velocities
+         * and the velocities of each particle according to the Euler-Cromer
+         * scheme.
+         *
+         * You can access the position of particle number i by
+         *
+         *      p->getPosition()
+         *
+         * Remember that the positions and velocities of all the particles are
+         * vec3 vectors, which you can use the +=, -=, etc. operators on.
+         */
+    }
+}
+
+std::string EulerCromer::getName() {
+    return "Euler-Cromer";
+}
