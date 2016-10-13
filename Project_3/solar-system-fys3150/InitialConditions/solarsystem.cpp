433e1b88529d6b42a67be2a962dd7b58af2cfadc
Richard Fauli <Richard Fauli>
2016-10-13 15:32:31 +0200

    Solar init

--- Project_3/solar-system-fys3150/InitialConditions/solarsystem.cpp
+++ Project_3/solar-system-fys3150/InitialConditions/solarsystem.cpp
@@ -16,7 +16,7 @@ void SolarSystem::setupParticles(System &system) {
      */
 
 
-    // sun mass 1.988544E-30 kg
+    // sun mass 1.988544E30 kg
 
     // initial positions for oct 12.
     Particle* Sun = new Particle(vec3(3.571526865010806E-03, 3.389038138932890E-03, 0), vec3(-1.970078468466432E-06*365, 6.846218005095042E-06*365, 0), 1.0);
@@ -34,7 +34,8 @@ void SolarSystem::setupParticles(System &system) {
     Particle* Saturn = new Particle(vec3(-2.282432370743082E+00, -9.770968263962711E+00, 0), vec3(5.126047128541801E-03*365, -1.286363002735174E-03*365, 0), 2.857965426*pow(10,-4));
     //Uranus
     Particle* Uranus = new Particle(vec3(1.846778022429348E+01, 7.551054732258933E+00, 0), vec3(1.846778022429348E+01*365, 7.551054732258933E+00*365, 0), 4.365510645*pow(10,-5));
-
+    //Venus
+    //Particle* Venus = new Particle(vec3(1.422942118656473E-01, -7.105570386923493E-01, 0), vec3(1.971755883600314E-02*365, 3.794686413925673E-03*365, 0), 2.448273712E-06);
     system.addParticle(Sun);
     system.addParticle(Earth);
     system.addParticle(Mars);
@@ -42,6 +43,7 @@ void SolarSystem::setupParticles(System &system) {
     system.addParticle(Neptune);
     system.addParticle(Saturn);
     system.addParticle(Uranus);
+    //system.addParticle(Venus);
 
 }
 
