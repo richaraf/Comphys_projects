46acb187a077c647ef028cd3752dcf16f3ca7cc6
Richard Fauli <Richard Fauli>
2016-10-13 11:39:47 +0200

    added examples::threbodysystem

--- Project_3/solar-system-fys3150/Potentials/newtoniangravity.cpp
+++ Project_3/solar-system-fys3150/Potentials/newtoniangravity.cpp
@@ -1,10 +1,6 @@
 #include "newtoniangravity.h"
 #include <iostream>
-<<<<<<< HEAD
 #include <cmath>
-=======
-#include<cmath>
->>>>>>> db2312042b41882a8a34a5756f44dbaa809e00c1
 
 NewtonianGravity::NewtonianGravity(double G) : m_G(G) {
 
@@ -33,10 +29,6 @@ void NewtonianGravity::computeForces(Particle& a, Particle& b) {
      * is only neccessary for verification purposes later.
      */
     vec3 dr = a.getPosition()-b.getPosition();
-<<<<<<< HEAD
-    double G = 4*pow(3.1415,2);
-=======
->>>>>>> db2312042b41882a8a34a5756f44dbaa809e00c1
     double drlength = dr.length();
     double drlength2 = dr.lengthSquared();
     double dFx = -m_G*(a.getMass()*b.getMass())/(drlength*drlength2)*b.getPosition()(0);
