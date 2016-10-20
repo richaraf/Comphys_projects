7302a96f999799b60e747a186116b95dc6a75985
Richard Fauli <Richard Fauli>
2016-10-20 12:47:30 +0200

    changed write to file and pointers

--- Project_3/solar-system-fys3150/examples.cpp
+++ Project_3/solar-system-fys3150/examples.cpp
@@ -17,66 +17,66 @@
 void Examples::twoBodyProblem() {
     double G            = 4*pow(M_PI,2);
 
-    System* twoBodySystem = new System();
-    twoBodySystem->setIntegrator        (new VelocityVerlet(twoBodySystem));
-    twoBodySystem->setDt                (1e-4);
-    twoBodySystem->setPotential         (new NewtonianGravity(G));
-    twoBodySystem->setInitialCondition  (new TwoBody());
-    twoBodySystem->setFileWriting       (true);
-    twoBodySystem->setTestVelocity      (true, 1); //(Run/not, tol)
-    twoBodySystem->removeLinearMomentum ();
-    twoBodySystem->integrate            (5000);
+    System twoBodySystem;
+    twoBodySystem.setIntegrator        (new VelocityVerlet(&twoBodySystem));
+    twoBodySystem.setDt                (1e-4);
+    twoBodySystem.setPotential         (new NewtonianGravity(G));
+    twoBodySystem.setInitialCondition  (new TwoBody());
+    twoBodySystem.setFileWriting       (true);
+    twoBodySystem.setTestVelocity      (true, 1); //(Run/not, tol)
+    twoBodySystem.removeLinearMomentum ();
+    twoBodySystem.integrate            (5000);
 }
 
 void Examples::threeBodyProblem() {
     double G = 4*pow(M_PI,2);
 
-    System* threeBodySystem = new System();
-    threeBodySystem->setIntegrator          (new VelocityVerlet(threeBodySystem));
-    threeBodySystem->setPotential           (new NewtonianGravity(G));
-    threeBodySystem->setInitialCondition    (new ThreeBody());
-    threeBodySystem->setFileWriting         (true);
-    threeBodySystem->removeLinearMomentum   ();
-    threeBodySystem->integrate              (5000);
+    System threeBodySystem;
+    threeBodySystem.setIntegrator          (new VelocityVerlet(&threeBodySystem));
+    threeBodySystem.setPotential           (new NewtonianGravity(G));
+    threeBodySystem.setInitialCondition    (new ThreeBody());
+    threeBodySystem.setFileWriting         (true);
+    threeBodySystem.removeLinearMomentum   ();
+    threeBodySystem.integrate              (5000);
 }
 
 void Examples::solarSystemProblem()
 {
     double G = 4*pow(M_PI,2);
 
-    System* solarSystem = new System();
-    solarSystem->setIntegrator          (new VelocityVerlet(solarSystem));
-    solarSystem->setDt                  (1e-3);
-    solarSystem->setPotential           (new NewtonianGravity(G));
-    solarSystem->setInitialCondition    (new SolarSystem());
-    solarSystem->setFileWriting         (true);
-    solarSystem->removeLinearMomentum   ();
-    solarSystem->integrate              (50000);
-    solarSystem->computeAngularMomentum ();
+    System solarSystem();
+    solarSystem.setIntegrator          (new VelocityVerlet(&solarSystem));
+    solarSystem.setDt                  (1e-3);
+    solarSystem.setPotential           (new NewtonianGravity(G));
+    solarSystem.setInitialCondition    (new SolarSystem());
+    solarSystem.setFileWriting         (true);
+    solarSystem.removeLinearMomentum   ();
+    solarSystem.integrate              (50000);
+    solarSystem.computeAngularMomentum ();
 }
 
 void Examples::twoBody_escape_velProblem() {
     double G            = 4*pow(M_PI,2);
 
-    System* twoBodySystem = new System();
-    twoBodySystem->printEscape          (true);
-    twoBodySystem->setIntegrator        (new VelocityVerlet(twoBodySystem));
-    twoBodySystem->setPotential         (new NewtonianGravity(G));
-    twoBodySystem->setInitialCondition  (new TwoBody_escape_vel());
-    twoBodySystem->setFileWriting       (true);
-    twoBodySystem->removeLinearMomentum ();
-    twoBodySystem->integrate            (5000);
+    System twoBodySystem;
+    twoBodySystem.printEscape          (true);
+    twoBodySystem.setIntegrator        (new VelocityVerlet(&twoBodySystem));
+    twoBodySystem.setPotential         (new NewtonianGravity(G));
+    twoBodySystem.setInitialCondition  (new TwoBody_escape_vel());
+    twoBodySystem.setFileWriting       (true);
+    twoBodySystem.removeLinearMomentum ();
+    twoBodySystem.integrate            (5000);
 }
 
 void Examples::mercurySunProblem() {
     double G            = 4*pow(M_PI,2);
-    System* mercurySunSystem = new System();
-    mercurySunSystem->setIntegrator        (new VelocityVerlet(mercurySunSystem));
-    mercurySunSystem->setPotential         (new NewtonianGravity(G));//(new RelativisticNewtonianGravity(G));
-    mercurySunSystem->setInitialCondition  (new MercurySun());//(new MercurySun());
-    mercurySunSystem->setDt                (1e-5);
-    mercurySunSystem->setFileWriting       (true);
-    mercurySunSystem->setFileWritingMercury(true);
-    mercurySunSystem->removeLinearMomentum ();
-    mercurySunSystem->integrate            (10000000);
+    System mercurySunSystem;
+    mercurySunSystem.setIntegrator        (new VelocityVerlet(&mercurySunSystem));
+    mercurySunSystem.setPotential         (new RelativisticNewtonianGravity(G));//(new RelativisticNewtonianGravity(G));
+    mercurySunSystem.setInitialCondition  (new MercurySun());//(new MercurySun());
+    mercurySunSystem.setDt                (1e-6);
+    mercurySunSystem.setFileWriting       (true);
+    mercurySunSystem.setFileWritingMercury(true);
+    mercurySunSystem.removeLinearMomentum ();
+    mercurySunSystem.integrate            (10000000);
 }
