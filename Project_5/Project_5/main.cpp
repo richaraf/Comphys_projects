307a9f03f11d5eb20c014a5e656fd1c6a8bc3fde
Even Marius Nordhagen <even.nordhagen@live.no>
2016-11-30 08:53:32 +0100

    3D and contour plots

--- Project_5/Project_5/main.cpp
+++ Project_5/Project_5/main.cpp
@@ -7,17 +7,17 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     //VariationalMethod(omega, N, trialversion, alpha, beta);
-    string filename = "../E_L2_file_omega=1.dat";
+    string filename = "../E_L2_file_omega=0_01.dat";
     ofstream ofs;
     ofs.open(filename, ofstream::out | ofstream::trunc);
     ofs.close();
 
     double alpha, beta;
-    for (int i = 0; i < 9; i++){
-        alpha = 0.70 + i*0.10;
-        for (int j = 0; j < 9; j++){
-            beta = 0.10 + j*0.05;
-            VariationalMethod(1.0, 1e7, 2, alpha, beta, filename);
+    for (int i = 0; i < 12; i++){
+        alpha = 0.7 + i*0.025;
+        for (int j = 0; j < 11; j++){
+            beta = 0.0 + j*0.05;
+            VariationalMethod(0.01, 1e7, 2, alpha, beta, filename);
         }
     }
     //outfile_EL1.close();
--- Project_5/plot_3D.py
+++ Project_5/plot_3D.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-infiles = ['E_L2_file_omega=1.dat']#, 'E_L2_file_omega=0_5.dat', 'E_L2_file_omega=0_01.dat']
+infiles = ['E_L2_file_omega=1.dat', 'E_L2_file_omega=0_5.dat', 'E_L2_file_omega=0_01.dat']
 
 number_of_columns = 4
-number_of_alphas  = 21
+number_of_alphas  = 12
 number_of_betas   = 11
 number_of_lines   = number_of_alphas*number_of_betas
 
@@ -13,6 +13,7 @@ data = np.zeros(shape=[len(infiles), number_of_lines, number_of_columns])
 
 i = 0
 for infile in infiles:
+    #---READ FILE---
     myfile = open(infile, 'r')
     j = 0
     for line in myfile:
@@ -21,7 +22,14 @@ for infile in infiles:
             data[i,j,k] = numbers[k]
         j += 1
 
-    label_size = {'size':'16'}
+    #---MINIMUM ENERGY---
+    minimum_energy = np.argmin(data[i,:,2])
+    a = (minimum_energy/number_of_betas)*number_of_alphas
+    b = minimum_energy%number_of_betas
+    minimum_alpha = data[i,a,0]
+    minimum_beta  = data[i,b,1]
+
+    #---MESHGRID---
     meshgrid  = np.zeros(shape=[number_of_columns, number_of_alphas, number_of_betas])
 
     for p in range(number_of_columns):
@@ -31,16 +39,20 @@ for infile in infiles:
                 meshgrid[p, u, v] = data[i, h, p]
                 h += 1
 
+    #---PLOT---
+    label_size = {'size':'16'}
     label_list = [r'Local energy, $E_{L2}$',
                   r'Energy variance, $\sigma_E^2$']
 
-    for j in range(number_of_columns-2):
+    for j in range(number_of_columns-3):
         plt.figure()
-        plt.contourf(meshgrid[0], meshgrid[1], meshgrid[j+2], 500)
-        plt.colorbar()
-        plt.title(label_list[j],**label_size)
+        plt.plot(minimum_alpha, minimum_beta,'ow',label='Minimum')
+        plt.contourf(meshgrid[0], meshgrid[1], meshgrid[j+2], 100)
+        cbar = plt.colorbar(orientation='vertical')
+        cbar.set_label(label_list[j],**label_size)
         plt.xlabel(r'Variational parameter, $\alpha$',**label_size)
         plt.ylabel(r'Variational parameter, $\beta$',**label_size)
+        plt.legend(loc='upper right')
 
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d') 
