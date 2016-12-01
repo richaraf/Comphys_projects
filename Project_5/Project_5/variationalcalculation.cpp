add786bb6228b62b8c3ebbf5228a3e3b413043da
Richard Fauli <Richard Fauli>
2016-12-01 11:47:19 +0100

    write Kaverage and Vaverage to file

--- Project_5/Project_5/variationalcalculation.cpp
+++ Project_5/Project_5/variationalcalculation.cpp
@@ -123,7 +123,7 @@ void VariationalMethod(double omega, int N, int trialversion, double alpha, doub
     E_sqrd_average = E_sqrd_tot/((1-eq)*(N-1));
     K_average = K_tot/((1-eq)*(N-1));
     V_average = V_tot/((1-eq)*(N-1));
-    outfile_EL2 << setprecision(15) << setw(5) << alpha  << ' ' << setw(5) << beta << ' ' << setw(20) << E_average << ' ' << setw(20) << E_sqrd_average - E_average*E_average << setw(20) << r12_average << endl;
+    outfile_EL2 << setprecision(15) << setw(5) << alpha  << ' ' << setw(5) << beta << ' ' << setw(20) << E_average << ' ' << setw(20) << E_sqrd_average - E_average*E_average << setw(20) << omega << setw(20) << r12_average << setw(20) << K_average << setw(20) << V_average << endl;
     outfile_EL2.close();
     cout <<" alpha: " << alpha << " beta: " << beta << " E_average: " << E_average << " Variance E: " << E_sqrd_average - E_average*E_average << endl;
     cout <<" Acc ratio: " << accepted/((double)((1-eq)*N)) << " r12_average: " << r12_average << endl;
