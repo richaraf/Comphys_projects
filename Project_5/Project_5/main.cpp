d9db4e5c837fee8f710255c73fcf06963f6f6210
Richard Fauli <Richard Fauli>
2016-12-06 12:16:43 +0100

    discussion and a table in result

--- Project_5/Project_5/main.cpp
+++ Project_5/Project_5/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     //VariationalMethod(omega, N, trialversion, alpha, beta);
-    string filename = "../E_L2_file_omegas.dat";
+    string filename = "../E_L1_file_omegas.dat";
     ofstream ofs;
     ofs.open(filename, ofstream::out | ofstream::trunc);
     ofs.close();
@@ -17,9 +17,17 @@ int main(int argc, char *argv[])
         alpha = 1.0 + i*0.01;
         for (int j = 0; j < 1; j++){
             beta = 0.25 + j*0.05;
-            for (int k = 0; k < 10; k++){
-                omega = 0.1 + 0.1*k;
-                VariationalMethod(omega, 1e6, 2, alpha, beta, filename);
+            for (int k = 0; k < 1; k++){
+                if (k == 0){
+                    omega = 0.1;
+                }
+                if (k == 1){
+                    omega = 0.5;
+                }
+                if (k == 2){
+                    omega = 1.0;
+                }
+                VariationalMethod(0.01, 1e7, 2, 0.9, 0.05, filename);
             }
         }
     }
